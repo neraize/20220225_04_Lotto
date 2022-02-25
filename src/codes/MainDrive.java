2b832ed0a6b8c79c28234215f5c00b3cb98634c2
seojungmin <neraize@gmail.com>
2022-02-25 12:16:46 +0900

    1~45사이의 숫자를 입력해야. 다음 단계로 넘어감

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -11,9 +11,18 @@ public class MainDrive {
 		Scanner myScanner = new Scanner(System.in);
 		
 		for(int i=0; i<6; i++) {
-			System.out.print((i+1)+"번째 숫자: ");
-			int inputNum =myScanner.nextInt();
+			
+			while(true) {
+				System.out.print((i+1)+"번째 숫자: ");
+				int inputNum =myScanner.nextInt();		
+				
+				boolean isRangeOk= (1<=inputNum) && (inputNum<=45);
+				
+				if(isRangeOk) {
+					break;
+				}
+				
+			}
 		}
-		
 	}
 }
